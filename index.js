4aa21bc2a1468449140dc2a54882e37469cf7a39
show0317sw <tunakan_8584@yahoo.co.jp>
2019-01-07 15:42:53 +0900

    :sparkles: リアクションの追加と操作を実装

--- index.js
+++ index.js
@@ -10,11 +10,16 @@ const Client = new Discord.Client();
 
 // 準備完了時の処理
 Client.on('ready', ()=>{
-  console.log(`Logged in ${Client.user.tag}`);
+  console.log(`ログイン ${Client.user.tag}`);
 });
 
 // エラー処理
 Client.on('error', console.error);
+
+// ログイン処理
+const token = process.env.DISCORD_BOT_TOKEN;
+Client.login(token)
+  .catch(console.error);
 
 // TODO 通信が安定しない｡bufferutilとか入れる｡
 
@@ -27,6 +32,7 @@ Client.on('message', m=>{
 
   const args = m.content.split(' ');
   const command = args.slice(1, 2).join().toLowerCase();
+  const guildId = m.guild.id;
 
   // joinコマンド
   if(command === 'join'){
@@ -34,13 +40,13 @@ Client.on('message', m=>{
     const channel = (m.guild.channels.get(channelId) || m.member.voiceChannel);
     if(channel && channel.type === 'voice'){
       channel.join().then(connection=>{
-        if(!queue.get(m.guild.id)){
+        if(!queue.get(guildId)){
           const voiceChannel = channel;
           const textChannel = m.channel;
-          queue.set(m.guild.id, {connection, voiceChannel, textChannel, controller: null, audio: [], playing: false});
+          queue.set(guildId, {connection, voiceChannel, textChannel, controller: null, status: {audio: [], playing: false, volume: 0.5}});
         }
         m.channel.send('接続しました');
-        controller(m);
+        controller(guildId);
       });
     }else{
       m.channel.send('ボイスチャンネルを指定してください');
@@ -49,9 +55,9 @@ Client.on('message', m=>{
 
   // leaveコマンド
   else if(command === 'leave'){
-    if(queue.get(m.guild.id)){
-      controller(m, true);
-      queue.get(m.guild.id).voiceChannel.leave();
+    if(queue.get(guildId)){
+      controller(guildId, true);
+      queue.get(guildId).voiceChannel.leave();
       m.channel.send('切断しました');
     }else{
       m.channel.send('ボイスチャンネルに接続されていません');
@@ -60,52 +66,105 @@ Client.on('message', m=>{
 });
 
 // コントローラー
-function controller(m, leave = false){
+function controller(guildId, leave = false){
   if(!leave){
-    console.log('コントローラー表示');
     const embed = new RichEmbed()
-      .setTitle('コントローラー')
-      .setDescription('リアクションで操作してください\n'
-        + ':play_pause: 再生/一時停止\n'
-        + ':stop_button: 停止\n'
-        + ':record_button: 録音\n'
-        + ':track_previous: 前のトラック\n'
-        + ':track_next: 次のトラック\n'
-        + ':speaker: 音量ダウン\n'
-        + ':loud_sound: 音量アップ\n'
-        + ':mute: ミュート')
+    .setTitle('コントローラー(現在一部機能使用不可)')
+    .setDescription('リアクションで操作してください\n'
+    + ':play_pause: 再生/一時停止\n'
+    + ':stop_button: 停止\n'
+    + ':record_button: 録音\n'
+    + ':track_previous: 前のトラック\n'
+    + ':track_next: 次のトラック\n'
+    + ':speaker: 音量ダウン\n'
+    + ':loud_sound: 音量アップ\n'
+    + ':mute: ミュート')
       .setColor('GOLD');
-    m.channel.send(embed)
+      queue.get(guildId).textChannel.send(embed)
       .then(message=>{
-        queue.get(m.guild.id).controller = message;
-        message.react('⏯').then(()=>{
-          message.react('⏹').then(()=>{
-            message.react('⏺').then(()=>{
-              message.react('⏮').then(()=>{
-                message.react('⏭').then(()=>{
-                  message.react('🔈').then(()=>{
-                    message.react('🔊').then(()=>{
-                      message.react('🔇');
-                    });
-                  });
-                });
-              });
+        queue.get(guildId).controller = message;
+        reactionManager(message)
+          .then(()=>{
+            Client.on('messageReactionAdd', (messageReaction, user)=>{
+              statusManager(messageReaction, user);
             });
-          });
-        });
+            Client.on('messageReactionRemove', (messageReaction, user)=>{
+              statusManager(messageReaction, user);
+            });
+          })
       })
       .catch(console.error);
-  }else{
-    console.log('コントローラー削除');
-    queue.get(m.guild.id).controller.delete()
+    }else{
+    queue.get(guildId).controller.delete()
       .then(()=>{
-        queue.delete(m.guild.id);
+        queue.delete(guildId);
       })
       .catch(console.error);
   }
 }
 
-// ログイン処理
-const token = process.env.DISCORD_BOT_TOKEN;
-Client.login(token)
-  .catch(console.error);
+// ステータスを制御する関数
+function statusManager(messageReaction, user){
+  if(messageReaction.me && user.username !== 'U20 Radio Bot' && queue.get(messageReaction.message.channel.guild.id).controller.id === messageReaction._emoji.reaction.message.id){
+    const guildId = messageReaction.message.channel.guild.id;
+    const reaction = messageReaction._emoji.name;
+    switch(reaction){
+      case '⏯':
+        console.log('再生/一時停止');
+        break;
+      case '⏹':
+        console.log('停止');
+        break;
+      case '⏺':
+        console.log('録音');
+        break;
+      case '⏮':
+        console.log('前のトラック');
+        break;
+      case '⏭':
+        console.log('次のトラック');
+        break;
+      case '🔈':
+        console.log('音量ダウン');
+        break;
+      case '🔊':
+        console.log('音量アップ');
+        break;
+      case '🔇':
+        console.log('ミュート');
+        break;
+      default:
+        console.log('未対応の操作');
+        break;
+    }
+  }
+}
+
+// リアクションをつける関数
+function reactionManager(m){
+  return new Promise((resolve, reject)=>{
+    m.react('⏯')
+      .then(()=>{
+        m.react('⏹')
+          .then(()=>{
+            m.react('⏺')
+              .then(()=>{
+                m.react('⏮')
+                  .then(()=>{
+                    m.react('⏭')
+                      .then(()=>{
+                        m.react('🔈')
+                          .then(()=>{
+                            m.react('🔊')
+                              .then(()=>{
+                                m.react('🔇')
+                                  .then(resolve);
+                              });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+}
